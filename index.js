b92ab78076c50c692b1a03aa40307c8bdf283ed4
Gerriss Gailey <gerrissgailey@gmail.com>
2021-02-05 23:42:45 -0700

    Adding API's to call for 5 Day forecase & UV Index. Still in progress.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // Declare Variables
 var citySearchInput = document.getElementById("city-search-input");
 var citySearchBtn = document.getElementById("city-search-btn");
-var hideList = document.getElementById("hide-list");
+// var hideList = document.getElementById("hide-list");
 var currentWeather = document.getElementById("current-weather");
+var todaysDate = moment().format("dddd, MMMM Do gggg")
 
 // Get localStorage items and display to Previous Searches
 
@@ -24,33 +25,63 @@ function callWeatherAPI() {
     // User response variables
     
 
-
+    // OpenWeatherMap API - Current Weather Data
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + citySearchInput + "&appid=" + apiKey + "&units=imperial";
-    // AJAX call OpenWeatherMap API
     
+    // AJAX call for Current Weather Data
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
         console.log(queryURL);
         console.log(response);
-        // Display weather API response to results page
-        // document.getElementById("current-weather").textContent = citySearchInput + " Weather";
-
-        // API response variables
-        $(".city").html("<h2>" + response.name + "</h2>"); // Need to add date code into this block
+        
+        // Display Current Weather Data response to page
+        $(".city").html("<h2>" + response.name + " (" + todaysDate + ")" + "</h2>"); // Need to add date code into this block
         $(".temperature").html("Temperature: " + response.main.temp + " &deg;F");
         $(".humidity").text("Humidity: " + response.main.humidity + " %");
         $(".wind").text("Wind Speed: " + response.wind.speed + " MPH");
-        // $(".uv-index").text("UV Index: " + response.main.humidity);
-        // var city
-        // var temperature
-        // var humidity
-        // var windSpeed
-        // var uvIndex
         
-        // Need to add in API for UV Index down here
+        // Latitude & longitude variables - pulled from Current Weather Data to use with UV Index call
+        var latitude = response.coord.lat;
+        var longitude = response.coord.lon;
+        console.log(latitude);
+        console.log(longitude);
+        
+        // OpenWeatherMap API - UV Index
+        // var queryURL2 = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
+        var queryURL2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=minutely,hourly,alerts&appid=" + apiKey + "&units=imperial";
+        
+        // AJAX call for UV Index
+        $.ajax({
+            url: queryURL2,
+            method: "GET"
+        }).then(function(responseUV) {
+            console.log(queryURL2);
+            console.log(responseUV);
+            $(".uv-index").text("UV Index: " + responseUV.current.uvi);
 
-        // Need to add in API for 5-day forecast down here
+            // OpenWeatherMap API - 5-Day Forecast
+            // var queryURL3 = "https://api.openweathermap.org/data/2.5/forecast?q=" + citySearchInput + "&appid=" + apiKey + "&units=imperial";
+            
+            // // AJAX call for 5-Day Forecast
+            // $.ajax({
+            //     url: queryURL3,
+            //     method: "GET"
+            // }).then(function(responseFiveDay) {
+            //     console.log(queryURL3);
+            //     console.log(responseFiveDay);
+
+
+            // })
+        })
+        
     })
 }
+
+
+
+
+// Need to add in API for UV Index down here
+
+// Need to add in API for 5-day forecast down here
